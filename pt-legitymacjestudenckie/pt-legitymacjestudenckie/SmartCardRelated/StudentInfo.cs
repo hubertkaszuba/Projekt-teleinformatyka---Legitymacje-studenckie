0ae7ddc406082aa7dfbba7ba4ebc59a2d18128b9
DKaczmarek <magiczny.dominik.k@gmail.com>
2018-03-26 21:01:59 +0200

    Dodano klase pomocniczą StudentInfo

---
+++ pt-legitymacjestudenckie/pt-legitymacjestudenckie/SmartCardRelated/StudentInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pt_legitymacjestudenckie.SmartCardRelated
+{
+    class StudentInfo
+    {
+        // Sparsowane z karty informacje o studencie
+        string firstName { get; }
+        string lastName { get; }
+        string index { get; }
+
+        // Rejestrowanie odbicia karty
+        DateTime timestamp { get; set; }
+
+        public StudentInfo(string fName, string lName, string idx, DateTime stamp)
+        {
+            firstName = fName;
+            lastName = lName;
+            index = idx;
+            timestamp = stamp;
+        }
+        
+
+    }
+}
