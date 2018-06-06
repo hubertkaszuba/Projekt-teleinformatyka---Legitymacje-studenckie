abe04144ae4c9c05f60a9a1b90cd5819e3ddc867
konradmichalak1 <konradmichalak96@gmail.com>
2018-06-06 19:13:47 +0200

    Edytor zajęć
    Dodano:
    - Nowe okno edycji zajęć
    - Filtrowanie zajęć pojedyńczych
    - Przygotowanie metod usuwających i edytujących zaznaczone z tabeli zajęcia.

--- pt-legitymacjestudenckie/pt-legitymacjestudenckie/ZajeciaForm.Designer.cs
+++ pt-legitymacjestudenckie/pt-legitymacjestudenckie/ZajeciaForm.Designer.cs
@@ -28,87 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgv_lista_obecnosci = new System.Windows.Forms.DataGridView();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.cb_student_przegladanie = new System.Windows.Forms.ComboBox();
-            this.cb_uwzglednij_date = new System.Windows.Forms.CheckBox();
-            this.label14 = new System.Windows.Forms.Label();
             this.btn_szukaj_obecnosci = new System.Windows.Forms.Button();
             this.cb_przedmiot_przegladanie = new System.Windows.Forms.ComboBox();
             this.label11 = new System.Windows.Forms.Label();
-            this.cb_data_przegladanie = new System.Windows.Forms.DateTimePicker();
+            this.cb_data_od = new System.Windows.Forms.DateTimePicker();
             this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.cb_data_do = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.data_zajec = new System.Windows.Forms.DateTimePicker();
+            this.godzina_zajec = new System.Windows.Forms.DateTimePicker();
+            this.btn_usun = new System.Windows.Forms.Button();
+            this.btn_aktualizuj = new System.Windows.Forms.Button();
+            this.Edytuj = new System.Windows.Forms.GroupBox();
+            this.cb_sala = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_lista_obecnosci)).BeginInit();
             this.groupBox5.SuspendLayout();
+            this.Edytuj.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgv_lista_obecnosci
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(338, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(450, 426);
-            this.dataGridView1.TabIndex = 0;
+            this.dgv_lista_obecnosci.AllowUserToAddRows = false;
+            this.dgv_lista_obecnosci.AllowUserToDeleteRows = false;
+            this.dgv_lista_obecnosci.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgv_lista_obecnosci.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_lista_obecnosci.Location = new System.Drawing.Point(336, 12);
+            this.dgv_lista_obecnosci.Name = "dgv_lista_obecnosci";
+            this.dgv_lista_obecnosci.ReadOnly = true;
+            this.dgv_lista_obecnosci.Size = new System.Drawing.Size(497, 426);
+            this.dgv_lista_obecnosci.TabIndex = 0;
+            this.dgv_lista_obecnosci.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_lista_obecnosci_CellClick);
             // 
             // groupBox5
             // 
-            this.groupBox5.Controls.Add(this.cb_student_przegladanie);
-            this.groupBox5.Controls.Add(this.cb_uwzglednij_date);
-            this.groupBox5.Controls.Add(this.label14);
+            this.groupBox5.Controls.Add(this.label2);
+            this.groupBox5.Controls.Add(this.label1);
             this.groupBox5.Controls.Add(this.btn_szukaj_obecnosci);
             this.groupBox5.Controls.Add(this.cb_przedmiot_przegladanie);
             this.groupBox5.Controls.Add(this.label11);
-            this.groupBox5.Controls.Add(this.cb_data_przegladanie);
+            this.groupBox5.Controls.Add(this.cb_data_do);
+            this.groupBox5.Controls.Add(this.cb_data_od);
             this.groupBox5.Controls.Add(this.label12);
-            this.groupBox5.Controls.Add(this.label13);
             this.groupBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.groupBox5.Location = new System.Drawing.Point(28, 87);
+            this.groupBox5.Location = new System.Drawing.Point(23, 30);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(295, 249);
+            this.groupBox5.Size = new System.Drawing.Size(295, 238);
             this.groupBox5.TabIndex = 8;
             this.groupBox5.TabStop = false;
-            this.groupBox5.Text = "Przeglądanie obecności";
-            // 
-            // cb_student_przegladanie
-            // 
-            this.cb_student_przegladanie.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.cb_student_przegladanie.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
-            this.cb_student_przegladanie.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
-            this.cb_student_przegladanie.FormattingEnabled = true;
-            this.cb_student_przegladanie.Location = new System.Drawing.Point(79, 110);
-            this.cb_student_przegladanie.Name = "cb_student_przegladanie";
-            this.cb_student_przegladanie.Size = new System.Drawing.Size(200, 23);
-            this.cb_student_przegladanie.TabIndex = 9;
-            // 
-            // cb_uwzglednij_date
-            // 
-            this.cb_uwzglednij_date.AutoSize = true;
-            this.cb_uwzglednij_date.Location = new System.Drawing.Point(132, 85);
-            this.cb_uwzglednij_date.Name = "cb_uwzglednij_date";
-            this.cb_uwzglednij_date.Size = new System.Drawing.Size(144, 20);
-            this.cb_uwzglednij_date.TabIndex = 8;
-            this.cb_uwzglednij_date.Text = "Uwzględnianie daty";
-            this.cb_uwzglednij_date.UseVisualStyleBackColor = true;
-            // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.label14.Location = new System.Drawing.Point(125, 140);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(135, 13);
-            this.label14.TabIndex = 7;
-            this.label14.Text = "wypełnić co najmniej jedno";
+            this.groupBox5.Text = "Zajęcia do edycji";
             // 
             // btn_szukaj_obecnosci
             // 
-            this.btn_szukaj_obecnosci.Location = new System.Drawing.Point(20, 165);
+            this.btn_szukaj_obecnosci.Location = new System.Drawing.Point(26, 143);
             this.btn_szukaj_obecnosci.Name = "btn_szukaj_obecnosci";
             this.btn_szukaj_obecnosci.Size = new System.Drawing.Size(244, 68);
             this.btn_szukaj_obecnosci.TabIndex = 6;
             this.btn_szukaj_obecnosci.Text = "Szukaj";
             this.btn_szukaj_obecnosci.UseVisualStyleBackColor = true;
+            this.btn_szukaj_obecnosci.Click += new System.EventHandler(this.btn_szukaj_obecnosci_Click);
             // 
             // cb_przedmiot_przegladanie
             // 
@@ -116,7 +99,7 @@
             this.cb_przedmiot_przegladanie.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.cb_przedmiot_przegladanie.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.cb_przedmiot_przegladanie.FormattingEnabled = true;
-            this.cb_przedmiot_przegladanie.Location = new System.Drawing.Point(79, 29);
+            this.cb_przedmiot_przegladanie.Location = new System.Drawing.Point(79, 24);
             this.cb_przedmiot_przegladanie.Name = "cb_przedmiot_przegladanie";
             this.cb_przedmiot_przegladanie.Size = new System.Drawing.Size(200, 23);
             this.cb_przedmiot_przegladanie.TabIndex = 2;
@@ -125,72 +108,199 @@
             // 
             this.label11.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(9, 33);
+            this.label11.Location = new System.Drawing.Point(9, 28);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(68, 16);
             this.label11.TabIndex = 0;
             this.label11.Text = "Przedmiot";
             this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // cb_data_przegladanie
+            // cb_data_od
             // 
-            this.cb_data_przegladanie.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.cb_data_przegladanie.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.cb_data_przegladanie.Enabled = false;
-            this.cb_data_przegladanie.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.cb_data_przegladanie.Location = new System.Drawing.Point(77, 62);
-            this.cb_data_przegladanie.Name = "cb_data_przegladanie";
-            this.cb_data_przegladanie.Size = new System.Drawing.Size(202, 20);
-            this.cb_data_przegladanie.TabIndex = 3;
+            this.cb_data_od.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cb_data_od.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.cb_data_od.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.cb_data_od.Location = new System.Drawing.Point(77, 76);
+            this.cb_data_od.Name = "cb_data_od";
+            this.cb_data_od.Size = new System.Drawing.Size(202, 20);
+            this.cb_data_od.TabIndex = 3;
             // 
             // label12
             // 
             this.label12.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(38, 64);
+            this.label12.Location = new System.Drawing.Point(9, 56);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(37, 16);
+            this.label12.Size = new System.Drawing.Size(40, 16);
             this.label12.TabIndex = 1;
-            this.label12.Text = "Data";
+            this.label12.Text = "Data:";
             this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label13
+            // cb_data_do
             // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(24, 114);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(53, 16);
-            this.label13.TabIndex = 5;
-            this.label13.Text = "Student";
+            this.cb_data_do.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cb_data_do.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.cb_data_do.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.cb_data_do.Location = new System.Drawing.Point(77, 102);
+            this.cb_data_do.Name = "cb_data_do";
+            this.cb_data_do.Size = new System.Drawing.Size(202, 20);
+            this.cb_data_do.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "od";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "do";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Data";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Godzina";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Sala";
+            // 
+            // data_zajec
+            // 
+            this.data_zajec.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.data_zajec.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.data_zajec.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.data_zajec.Location = new System.Drawing.Point(62, 32);
+            this.data_zajec.Name = "data_zajec";
+            this.data_zajec.Size = new System.Drawing.Size(215, 20);
+            this.data_zajec.TabIndex = 3;
+            // 
+            // godzina_zajec
+            // 
+            this.godzina_zajec.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.godzina_zajec.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.godzina_zajec.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.godzina_zajec.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.godzina_zajec.Location = new System.Drawing.Point(62, 61);
+            this.godzina_zajec.Name = "godzina_zajec";
+            this.godzina_zajec.Size = new System.Drawing.Size(66, 20);
+            this.godzina_zajec.TabIndex = 3;
+            // 
+            // btn_usun
+            // 
+            this.btn_usun.Enabled = false;
+            this.btn_usun.Location = new System.Drawing.Point(66, 129);
+            this.btn_usun.Name = "btn_usun";
+            this.btn_usun.Size = new System.Drawing.Size(75, 23);
+            this.btn_usun.TabIndex = 14;
+            this.btn_usun.Text = "Usuń";
+            this.btn_usun.UseVisualStyleBackColor = true;
+            this.btn_usun.Click += new System.EventHandler(this.btn_usun_Click);
+            // 
+            // btn_aktualizuj
+            // 
+            this.btn_aktualizuj.Enabled = false;
+            this.btn_aktualizuj.Location = new System.Drawing.Point(147, 129);
+            this.btn_aktualizuj.Name = "btn_aktualizuj";
+            this.btn_aktualizuj.Size = new System.Drawing.Size(75, 23);
+            this.btn_aktualizuj.TabIndex = 14;
+            this.btn_aktualizuj.Text = "Aktualizuj";
+            this.btn_aktualizuj.UseVisualStyleBackColor = true;
+            this.btn_aktualizuj.Click += new System.EventHandler(this.btn_aktualizuj_Click);
+            // 
+            // Edytuj
+            // 
+            this.Edytuj.Controls.Add(this.cb_sala);
+            this.Edytuj.Controls.Add(this.label3);
+            this.Edytuj.Controls.Add(this.btn_aktualizuj);
+            this.Edytuj.Controls.Add(this.data_zajec);
+            this.Edytuj.Controls.Add(this.btn_usun);
+            this.Edytuj.Controls.Add(this.godzina_zajec);
+            this.Edytuj.Controls.Add(this.label5);
+            this.Edytuj.Controls.Add(this.label4);
+            this.Edytuj.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.Edytuj.Location = new System.Drawing.Point(23, 274);
+            this.Edytuj.Name = "Edytuj";
+            this.Edytuj.Size = new System.Drawing.Size(295, 164);
+            this.Edytuj.TabIndex = 15;
+            this.Edytuj.TabStop = false;
+            this.Edytuj.Text = "Edytuj";
+            // 
+            // cb_sala
+            // 
+            this.cb_sala.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cb_sala.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cb_sala.FormattingEnabled = true;
+            this.cb_sala.Location = new System.Drawing.Point(62, 88);
+            this.cb_sala.Name = "cb_sala";
+            this.cb_sala.Size = new System.Drawing.Size(183, 23);
+            this.cb_sala.TabIndex = 25;
             // 
             // ZajeciaForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(845, 455);
+            this.Controls.Add(this.Edytuj);
             this.Controls.Add(this.groupBox5);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgv_lista_obecnosci);
             this.Name = "ZajeciaForm";
-            this.Text = "ZajeciaForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Text = "Edytor zajęć";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_lista_obecnosci)).EndInit();
             this.groupBox5.ResumeLayout(false);
             this.groupBox5.PerformLayout();
+            this.Edytuj.ResumeLayout(false);
+            this.Edytuj.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgv_lista_obecnosci;
         private System.Windows.Forms.GroupBox groupBox5;
-        private System.Windows.Forms.ComboBox cb_student_przegladanie;
-        private System.Windows.Forms.CheckBox cb_uwzglednij_date;
-        private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Button btn_szukaj_obecnosci;
         private System.Windows.Forms.ComboBox cb_przedmiot_przegladanie;
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.DateTimePicker cb_data_przegladanie;
+        private System.Windows.Forms.DateTimePicker cb_data_od;
         private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker cb_data_do;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker data_zajec;
+        private System.Windows.Forms.DateTimePicker godzina_zajec;
+        private System.Windows.Forms.Button btn_usun;
+        private System.Windows.Forms.Button btn_aktualizuj;
+        private System.Windows.Forms.GroupBox Edytuj;
+        private System.Windows.Forms.ComboBox cb_sala;
     }
 }
--- pt-legitymacjestudenckie/pt-legitymacjestudenckie/ZajeciaForm.cs
+++ pt-legitymacjestudenckie/pt-legitymacjestudenckie/ZajeciaForm.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
+using System.Data.Sql;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,166 @@ namespace pt_legitymacjestudenckie
 {
     public partial class ZajeciaForm : Form
     {
-        public ZajeciaForm()
+        TheConjuring_dbEntities1 conjuring;
+        SqlConnection connection;
+        Wykladowca wykladowca;
+        DatabaseController databaseController;
+        List<Zajecia_pojedyncze> lista_zajec_pojedynczych = new List<Zajecia_pojedyncze>();
+        Przedmiot przedmiotSzukany;
+        public ZajeciaForm(TheConjuring_dbEntities1 conjuring, SqlConnection connection, Wykladowca wykladowca)
         {
             InitializeComponent();
+            this.conjuring = conjuring;
+            this.connection = connection;
+            this.wykladowca = wykladowca;
+            databaseController = new DatabaseController();
+            RefreshComboboxes_PrzegladanieObecnosci();
+        }
+
+
+
+        private void btn_szukaj_obecnosci_Click(object sender, EventArgs e)
+        {
+            Zajecia_pojedyncze zajecia_Pojedyncze = new Zajecia_pojedyncze();
+            Zajecia_pojedyncze zajecia_Zedytowane = new Zajecia_pojedyncze();
+            Przedmiot przedmiot = null;
+            Zajecia zajecia = new Zajecia();
+            DateTime data_od, data_do;
+        
+            
+            //Przygotowanie daty w zakresie której będzie sprawdzana obecność
+            data_od = cb_data_od.Value;
+            data_do = cb_data_do.Value;
+            //Przygotowanie obiektu przedmiotu do sprawdzenia
+            if (cb_przedmiot_przegladanie.Text != "")
+            {
+                przedmiot = conjuring.Przedmiot.Where(o => (o.Nazwa == cb_przedmiot_przegladanie.Text)).FirstOrDefault();
+            }
+            if (cb_przedmiot_przegladanie.Text == "")
+            {
+                MessageBox.Show("Wypełnij pola!");
+            }
+            else
+            {
+                Znajdz_przedmiot_szukany();
+                zajecia = przedmiotSzukany.Zajecia.FirstOrDefault();
+                lista_zajec_pojedynczych = databaseController.Pojedyncz_od_do(conjuring, cb_data_od.Value, cb_data_do.Value, zajecia, wykladowca);
+                dgv_lista_obecnosci.DataSource = FillDataTable(lista_zajec_pojedynczych);
+            }
+
+        }
+        /// <summary>
+        /// Tworzy obiekt tabeli dla zajec_pojedynczych
+        /// </summary>
+        /// <returns></returns>
+        private DataTable Create()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Przedmiot");
+            dt.Columns.Add("Data");
+            dt.Columns.Add("Godzina");
+            dt.Columns.Add("Sala");
+            return dt;
+        }
+        private DataTable FillDataTable(List<Zajecia_pojedyncze> lista_zajec_pojedynczych)
+        {
+            DataTable dt = Create();
+
+            foreach (Zajecia_pojedyncze s in lista_zajec_pojedynczych)
+            {
+                DataRow new_row = dt.NewRow();
+                new_row["Przedmiot"] = s.Zajecia.Przedmiot.Nazwa;
+                new_row["Data"] = s.Data_zajec.ToLongDateString();
+
+                DateTime pom = (DateTime)s.Zajecia.Czas;
+                new_row["Godzina"] = pom.ToShortTimeString();
+                new_row["Sala"] = s.Zajecia.Sala.Numer + " " + s.Zajecia.Sala.Budynek;
+                dt.Rows.Add(new_row);
+            }
+            return dt;
+        }
+        /// <summary> Odświeża zawartość comboboxów w zakładce Przeglądanie obecności </summary>
+        private void RefreshComboboxes_PrzegladanieObecnosci()
+        {
+            cb_data_od.Value = DateTime.Now.AddMonths(-1);
+            cb_przedmiot_przegladanie.Items.Clear();
+            List<Przedmiot> przedmioty = databaseController.ListPrzedmiot_Zalogowanego(conjuring, wykladowca);
+            foreach (Przedmiot x in przedmioty)
+            {
+                cb_przedmiot_przegladanie.Items.Add(x.Nazwa);
+            }
+            //sale
+            cb_sala.Items.Clear();
+            List<Sala> sale = databaseController.ListSala(conjuring);
+            foreach (Sala x in sale)
+            {
+                cb_sala.Items.Add(x.Numer + " " + x.Budynek);
+            }
+        }
+
+        private void Znajdz_przedmiot_szukany()
+        {
+            List<Przedmiot> przedmioty = databaseController.ListPrzedmiot_Zalogowanego(conjuring, wykladowca);
+            Zajecia pom;
+            foreach (Przedmiot x in przedmioty)
+            {
+                pom = x.Zajecia.FirstOrDefault();   
+                if(pom.Przedmiot.Nazwa == cb_przedmiot_przegladanie.Text)
+                {
+                    przedmiotSzukany = x;
+                }
+            }
+        }
+
+        /* Aby zaznaczać cały wiersz w tabeli */
+        private int CurrentIndex=0;
+        private void dgv_lista_obecnosci_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btn_usun.Enabled = true;
+            btn_aktualizuj.Enabled = true;
+            CurrentIndex = dgv_lista_obecnosci.CurrentRow.Index;
+            data_zajec.Text = dgv_lista_obecnosci.CurrentRow.Cells[1].Value.ToString();
+            godzina_zajec.Text = dgv_lista_obecnosci.CurrentRow.Cells[2].Value.ToString();
+            cb_sala.SelectedIndex = cb_sala.FindStringExact(dgv_lista_obecnosci.CurrentRow.Cells[3].Value.ToString());
+        }
+
+        private void btn_usun_Click(object sender, EventArgs e)
+        {
+            Zajecia_pojedyncze zajecia_Pojedyncze = lista_zajec_pojedynczych[CurrentIndex];
+            try
+            {
+                //Tutaj wywołanie metody:
+                //      NazwaUsuwającejMetody(Zajecia_pojedyncze zajecia_Pojedyncze);
+                //Jedynym argumentem przekazywanym ode mnie są zajęcia_Pojedyncze, które są zajęciami zaznaczonymi przez użytkownika w tabeli.
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void btn_aktualizuj_Click(object sender, EventArgs e)
+        {
+            Zajecia_pojedyncze zajecia_Pojedyncze = lista_zajec_pojedynczych[CurrentIndex];
+
+            DateTime NowaDataZajec = Convert.ToDateTime(data_zajec.Text);
+            DateTime NowaGodzinaZajec = Convert.ToDateTime(godzina_zajec.Text);
+            
+            List<Sala> sale = databaseController.ListSala(conjuring);
+            Sala NowaSala = sale[cb_sala.SelectedIndex];
+
+            try
+            {
+                //Tutaj wywołanie metody:
+                //     NazwaMetody(Zajecia_pojedyncze zajecia_Pojedyncze, DateTime NowaDataZajec, DateTime NowaGodzinaZajec, Sala NowaSala);
+                // NowaDataZajec to jest to co jest pod: obiektZajęćPojedyńczych.Data_zajec
+                // NowaGodzinaZajec to jest to co jest pod: obiektZajęćPojedyńczych.Zajecia.Czas
+                // NowaSala to jest to co jest pod: obiektZajęćPojedyńczych.Zajecia.Sala
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
